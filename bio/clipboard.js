292c119dbd9daec89aed7dcc2012d423c1123695
Bhargavi Balakrishnan <140436667+bhargavibalakrishnan@users.noreply.github.com>
2023-08-19 19:46:50 -0500

    Create clipboard.js

---
+++ bio/clipboard.js
@@ -0,0 +1,16 @@
+// Reference of the paragraph tag
+const content = document.getElementById("content");
+
+// Reference of the copy button
+const copyBtn = document.getElementById("copy");
+
+// Copy button's onclick handler
+copyBtn.onclick = copyToClipboard;
+
+// Method responsible for copying the content
+function copyToClipboard() {
+    navigator.clipboard
+        .writeText(content.innerText)
+        .then(() => alert("Copied to clipboard"))
+        .catch((e) => alert(e.message));
+}
